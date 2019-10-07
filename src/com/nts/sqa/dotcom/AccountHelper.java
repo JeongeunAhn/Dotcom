e3d2a928c62038a1b531788ba515bdb823b30c04
jeongeun <huyu0121@gmail.com>
2019-10-07 15:06:38 +0900

    로그인,계정등록

--- src/com/nts/sqa/dotcom/AccountHelper.java
+++ src/com/nts/sqa/dotcom/AccountHelper.java
@@ -16,79 +16,87 @@ public class AccountHelper {
 	String id = null, password = null;
 	String[] splitedStr = null;
 	boolean id_exist = false;
-	
+
 	public void accountcheck(int option) {
 		// 계정이 있는 경우
-		Scanner sc = new Scanner(System.in);
-		
 		if (option == 1) {
-			loginloop: while (validaccount == false) { // ID와 패스워드가 일치할 때까지 입력받는다.
-				System.out.println("ID를 입력하세요.");
-				System.out.print("ID : ");
-				id = sc.next();
-				System.out.println("Password를 입력하세요");
-				System.out.print("PW : ");
-				password = sc.next();
-				try {
-					// 파일 객체 생성
-					File file = new File("C:\\Users\\User\\eclipse-workspace\\DotCom\\user.txt");
-					// 입력 스트림 생성
-					FileReader filereader = new FileReader(file);
-					// 입력 버퍼 생성
-					BufferedReader bufReader = new BufferedReader(filereader);
-
-					String line = null;
-					splitedStr = null;
-
-					while ((line = bufReader.readLine()) != null) { // 한줄씩 읽기
-						splitedStr = null;
-						splitedStr = line.split("\t"); // 탭을 기준으로 나눠준다
-
-						for (int i = 0; i < splitedStr.length; i++) {
-							splitedStr[i] = splitedStr[i].trim();
-						}
-						if (splitedStr[0].equals(id)) { // 아이디 존재
-							id_exist = true;
-							if (splitedStr[1].equals(password)) { // 아이디와 패스워드가 일치하면
-								validaccount = true;
-								break; // 파일읽기 끝
-							} else {// 아이디는 있는데 패스워드는 틀린경우
-								System.out.println("비밀번호가 틀립니다. 다시 입력해주세요");
-								break;
-							}
-						}
-					}
-					bufReader.close();
-
-					if (id_exist == false) {
-						System.out.println("입력하신 ID가 존재하지 않습니다.");
-						System.out.println("다시 로그인 하시려면 1번, 계정을 만들고 싶으시면 2번을 눌러주세요.");
-						while (true) {
-							try {
-								option = sc.nextInt();
-								if (option == 1) {
-									break;
-								} else if (option == 2) {
-									break loginloop;
-								} else {
-									System.out.println("1또는 2를 입력해주세요.");
-								}
-							} catch (InputMismatchException e) {
-								System.out.println("1 또는 2를 입력해주세요.");
-							}
-						}
-					}
-				} catch (FileNotFoundException e) {
-					System.out.println("파일을 찾을 수 없습니다. 다시 실행해주세요.");
-				} catch (IOException e) {
-					System.out.println("에러가 발생했습니다. 다시 실행해주세요.");
-				}
-			}
+			login();
 		}
 		// 계정 만들기
 		else if (option == 2) {
 			makeaccount();
 		}
+	}
+
+	public void login() {
+		int flag = 0;
+		Scanner sc = new Scanner(System.in);
+		loginloop: while (validaccount == false) { // ID와 패스워드가 일치할 때까지 입력받는다.
+			System.out.println("ID를 입력하세요.");
+			System.out.print("ID : ");
+			id = sc.next();
+			System.out.println("Password를 입력하세요");
+			System.out.print("PW : ");
+			password = sc.next();
+			try {
+				// 파일 객체 생성
+				File file = new File("C:\\Users\\User\\eclipse-workspace\\DotCom\\user.txt");
+				// 입력 스트림 생성
+				FileReader filereader = new FileReader(file);
+				// 입력 버퍼 생성
+				BufferedReader bufReader = new BufferedReader(filereader);
+
+				String line = null;
+				splitedStr = null;
+
+				while ((line = bufReader.readLine()) != null) { // 한줄씩 읽기
+					splitedStr = null;
+					splitedStr = line.split("\t"); // 탭을 기준으로 나눠준다
+
+					for (int i = 0; i < splitedStr.length; i++) {
+						splitedStr[i] = splitedStr[i].trim();
+					}
+					if (splitedStr[0].equals(id)) { // 아이디 존재
+						id_exist = true;
+						if (splitedStr[1].equals(password)) { // 아이디와 패스워드가 일치하면
+							validaccount = true;
+							break; // 파일읽기 끝
+						} else {// 아이디는 있는데 패스워드는 틀린경우
+							System.out.println("비밀번호가 틀립니다. 다시 입력해주세요");
+							break;
+						}
+					}
+				}
+				bufReader.close();
+
+				if (id_exist == false) {
+					System.out.println("입력하신 ID가 존재하지 않습니다.");
+					System.out.println("다시 로그인 하시려면 1번, 계정을 만들고 싶으시면 2번을 눌러주세요.");
+					while (true) {
+						try {
+							int option = sc.nextInt();
+							if (option == 1) {
+								break;
+							} else if (option == 2) {
+								flag = 1;
+								break loginloop;
+							} else {
+								System.out.println("1또는 2를 입력해주세요.");
+							}
+						} catch (InputMismatchException e) {
+							System.out.println("1 또는 2를 입력해주세요.");
+						}
+					}
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println("파일을 찾을 수 없습니다. 다시 실행해주세요.");
+			} catch (IOException e) {
+				System.out.println("에러가 발생했습니다. 다시 실행해주세요.");
+			}
+		}
+		// 중간에서 계정만들기를 희망한 경우
+		if (flag == 1)
+			makeaccount();
 	}
 
 	public void makeaccount() {
@@ -156,6 +164,7 @@ public class AccountHelper {
 						FileWriter fw = new FileWriter(file, true);
 						fw.write("\r\n" + id + "\t" + password);
 						fw.close();
+						break;
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
@@ -167,6 +176,7 @@ public class AccountHelper {
 				System.out.println("3~16자 사이로 입력해야 합니다.");
 			}
 		}
+		login();
 	}
 
 }
